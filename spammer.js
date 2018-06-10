17fc81f29fd369872c3ff196a1689d04ab8f3514
Milad Mehri <memm.milad@gmail.com>
2018-06-09 19:13:32 -0700

    Update and rename spammer.html to spammer.js

--- spammer.js
+++ spammer.js
@@ -1,5 +1,4 @@
-<!DOCTYPE html>
-<script>  
+
 var id = prompt("What is the studio ID?");
 var start = Math.floor(Math.random() * 228297019); 
 var end = start+500;
@@ -7,4 +6,3 @@ for(i=start; i < end; i++){
 $.ajax({type: "PUT",url: "https://scratch.mit.edu/site-api/projects/in/" + id + "/add/?pks=" + i,data: "pks=" + i});
 }
 $.ajax({type: "PUT",url: "https://scratch.mit.edu/site-api/projects/in/" + id + "/remove/?pks=" + i,data: "pks=" + i});
-</script>
